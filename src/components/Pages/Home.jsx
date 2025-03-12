c2a2508f3c016db474134fd2ce6a4e057d730d05
IamArjit <arjitacharyaganesh@gmail.com>
2025-03-12 13:28:14 +0530

    Build the second section

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -26,93 +26,91 @@ const Home = () => {
         alert(data);
       })
       .catch((error) => console.log(error));
-
-    // if (!name || !email || !phone) {
-    //   alert("All fields are required.");
-    //   return;
-    // }
-
-    // const formData = { name, email, phone };
-
-    // try {
-    //   setIsSubmitting(true);
-    //   await axios.post(
-    //     "https://script.google.com/macros/s/AKfycby1vzCdkObkDgmeYeWxp5ijxCn8pAJL92z0whVLMDG7geEISeK79zVhcRsXu71QE6iN/exec",
-    //     formData
-    //   );
-    //   setName("");
-    //   setEmail("");
-    //   setPhone("");
-    //   alert("Form data submitted successfully!");
-    // } catch (error) {
-    //   console.error(error);
-    //   alert("There was an error submitting your form. Please try again.");
-    // } finally {
-    //   setIsSubmitting(false);
-    // }
   };
 
   return (
-    <div className="home-container">
-      <div className="content-container">
-        <div className="left-content">
-          <h1>INDIA’S BEST ASTROLOGER HANISH BAGGA</h1>
-          <p>
-            Astrologer Hanish Bagga, founder & director of “Acharya Ganesh” is a
-            distinguished figure in the field of Astrology, known for
-            transforming the domains of astrology, vastu, and other disciplines.
-            He has worked in the astrological field for more than a decade and
-            is well-known and respected for his unique methods and profound
-            insights…
-          </p>
-          <button className="cta-button" onClick={() => navigate("/contact")}>
-            Book Appointment <FaArrowRightLong />
-          </button>
+    <>
+      <div className="home-container">
+        <div className="content-container">
+          <div className="left-content">
+            <h1>INDIA’S BEST ASTROLOGER HANISH BAGGA</h1>
+            <p>
+              Astrologer Hanish Bagga, founder & director of “Acharya Ganesh” is
+              a distinguished figure in the field of Astrology, known for
+              transforming the domains of astrology, vastu, and other
+              disciplines. He has worked in the astrological field for more than
+              a decade and is well-known and respected for his unique methods
+              and profound insights…
+            </p>
+            <button className="cta-button" onClick={() => navigate("/contact")}>
+              Book Appointment <FaArrowRightLong />
+            </button>
+          </div>
+          <div className="right-content">
+            <form className="astrology-form" onSubmit={handleSubmit}>
+              <h2>Get In Touch</h2>
+              <label htmlFor="name">
+                Name<span className="star">*</span>
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Enter your name..."
+              />
+              <label htmlFor="email">
+                Email<span className="star">*</span>
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter your email..."
+              />
+              <label htmlFor="number">
+                Phone No<span className="star">*</span>
+              </label>
+              <PhoneInput
+                className="phoneinput"
+                defaultCountry="in"
+                value={phone}
+                onChange={(phone) => setPhone(phone)}
+              />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
         </div>
-        <div className="right-content">
-          <form className="astrology-form" onSubmit={handleSubmit} action={""}>
-            <h2>Get In Touch</h2>
-            <label htmlFor="name">
-              Name<span className="star">*</span>
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Enter your name..."
-            />
-
-            <label htmlFor="email">
-              Email<span className="star">*</span>
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Enter your email..."
-            />
-
-            <label htmlFor="number">
-              Phone No<span className="star">*</span>
-            </label>
-            <PhoneInput
-              className="phoneinput"
-              defaultCountry="in"
-              value={phone}
-              onChange={(phone) => setPhone(phone)}
-            />
-            <button type="submit">Submit</button>
-          </form>
+        <div className="image-container">
+          <img src="./astrology.avif" alt="Astrology" />
         </div>
       </div>
-      <div className="image-container">
-        <img src="./astrology.avif" alt="Astrology" />
+      <div className="second-image-container">
+        <img src="./second.avif" alt="Second_Image" />
       </div>
-    </div>
+
+      {/* Box containing the images */}
+      <div className="image-box">
+        <div className="image-box-item">
+          <img src="calendar.png" alt="Calender_Logo" />
+          <p className="statistic-number">15+</p>
+          <p className="statistic-label">YEARS OF EXPERIENCE</p>
+        </div>
+        <div className="image-box-item">
+          <img src="tv.png" alt="Tv_Logo" />
+          <p className="statistic-number">3 Lac+</p>
+          <p className="statistic-label">LIVES TOUCHED</p>
+        </div>
+        <div className="image-box-item">
+          <img src="video.png" alt="Video_Logo" />
+          <p className="statistic-number">700+</p>
+          <p className="statistic-label">VIDEOS</p>
+        </div>
+      </div>
+    </>
   );
 };
 
